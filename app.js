874e705742984024bb2e37b188d9a2a15f0cac9d
Nick Howell <nlhowell12@gmail.com>
2018-04-17 12:38:28 -0400

    done but can't get styling to work

--- app.js
+++ app.js
@@ -11,32 +11,19 @@ const app = express();
 
 const port = 3000;
 
-app.use(express.static('./public'));
+app.use(express.static('./public/uploads'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 const path = './public/uploads';
 fs.readdir(path, (err, items) => {
-    let imageDisplay = [];
-    for (let i = 0; i < items.length; i++) {
-        if (items[i] != ".DS_Store") {
-        imageDisplay += `<img src= /uploads/${items[i]}>`;
-    }}
-    res.send(` <link rel="stylesheet" type="text/css" media="screen" href="app.css"/>
-    <body>
-    <h1>Kenziegram</h1>
-    <form id= "uploadForm" action="http://localhost:3000/upload" enctype="multipart/form-data" method="POST">
-        <input id="fileSelect" type="file" name="pic" accept="image/*">
-        <input id="uploadBut" type="submit" value="Upload">
-        
-    </form>
-    <div id="imageDisplay">${imageDisplay}</div>
-</body>`);
+    res.render('index', {title: 'Kenzigram', images: items});
 });
 });
 
 app.post('/upload', upload.single('pic'), (req, res, next) => {
-    res.send('<p id="uploadMessage">Uploaded photo! </p><form action="http://localhost:3000" method="get"><input type="submit" value="Return to gallery"></form>')}
-    );
+    res.render('upload');
+});
 
 
 
